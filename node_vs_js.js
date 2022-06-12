18d0f1bc95908f87da7c2af1dfa08c44f0a647f3
naamagi <n.gilat@gmail.com>
2022-06-12 13:22:56 +0300

    1.1

---
+++ node_vs_js.js
@@ -0,0 +1,15 @@
+// Node.js features:
+// Node.js has the global object, 
+// it is not limited to the browser (oppose to JS, since it is it written largely with c++)
+// it allows to run as a stand alone process, it allows to run js on a server and to compile js code (js is limited to the browser)
+// provides libraries
+//  provides binding -when it extends the V8 engine that allows node.js to interact with the file system
+//  node.js allows us to build servers , command line interfaces, back ends
+//  extends the functionality of js runtime,
+//   has 'process' that allows to manipulate the node processes that's running
+
+
+// Javascript features:
+// in js you can add click events, 
+// it has document to manipulate the dom(since in the browser we have a dom), 
+// it has the window object ( node doesn't need a window)
